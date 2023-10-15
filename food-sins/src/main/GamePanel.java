b4d23ba3e9f939e6234b7c6f77153864f53a4472
hushpuppy <140564473+hushpuppiesss@users.noreply.github.com>
2023-10-15 12:47:16 -0400

    Add files via upload

---
+++ food-sins/src/main/GamePanel.java
@@ -0,0 +1,115 @@
+package main;
+
+import entity.Player;
+import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.JFrame;
+// java awt, api for developing gui
+
+public class GamePanel extends JPanel implements Runnable{
+    // game panel works kind of like a game screen
+    // SCREEN SETTINGS
+    // 32x32 tiles
+    final int originalTileSize = 32;
+    // scale factor
+    final int scale = 2;
+
+    // the size that you'll see on screen - 64x tiles
+    public final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16; // n tiles wide
+    final int maxScreenRow = 10; // n tiles tall
+
+    final int screenWidth = tileSize * maxScreenCol; // 1920 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 1088 pixels.
+
+    // instantiating the key handler
+    KeyHandler keyH = new KeyHandler();
+    // creating the game clock
+    Thread gameThread;
+
+    // FPS
+    int FPS = 32;
+    // Allows for usage of key handler and game panel fom player.jav
+
+    Player player = new Player(this,keyH);
+
+    // setting the player's default position, where does the player spawn?
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+    //Game State
+    public  int gameState;
+    public final int playState = 1;
+    public final int pauseState = 2;
+
+
+    // screen setup
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK );
+        this.setDoubleBuffered(true); // improves rendering performance as drawing will be done in an offscreen painting buffer
+        this.addKeyListener(keyH); // so that game panel can recognize key input
+        this.setFocusable(true); // "focused" to receive input
+    }
+
+
+    // game thread
+    public void startGameThread() {
+        gameThread = new Thread (this); // passing game panel to thread constructor, initiating the thread
+        gameThread.start(); // will call the run method
+    }
+
+    // when we start a game thread, this run method is called
+    @Override
+    public void run() {
+
+        // sleep method
+        double drawInterval = (double) 1000000000 / FPS; // draw the screen this amount of times per second
+        double nextDrawTime = System.nanoTime() + drawInterval; // next draw time will be hte current time plus
+                                                                // draw interval seconds later
+
+        // core game loop
+        while (gameThread != null) {
+
+            // 1 UPDATE : updating information such as character positions
+            update();
+            // 2 DRAW: drawing screen with the updated info
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime(); // document this later
+                remainingTime = remainingTime/1000000;
+
+                if (remainingTime< 0){
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+    public void update() { // to update information
+
+
+        player.update();
+    }
+    public void paintComponent(Graphics g)  {
+    /* paintComponent is a standard method to draw things on jpanel, Graphics is a class that
+    has many functions to draw objects on the screen */
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        player.draw(g2);
+        g2.dispose(); // disposes of graphic context &   releases systems resources to save memory
+
+    }
+
+}
