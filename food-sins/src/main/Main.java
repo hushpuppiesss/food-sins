6e911a0a33bd959a0f18112d5dcd80633355834d
hushpupiesss <rachelquedding1611@gmail.com>
2023-11-17 17:31:00 -0500

    fixed icon and resized frame icon.png

--- food-sins/src/main/Main.java
+++ food-sins/src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import main.GamePanel;
 import javax.swing.*;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,20 +11,19 @@ public class Main {
         JFrame window = new JFrame(); // creates a frame
         GamePanel gamePanel = new GamePanel();
 
-        String frameIconPath = "/res/menu/frame icon.png";
-        ImageIcon frameIcon = new ImageIcon(frameIconPath); // creating icon for the frame
+        String frameIconPath = "/res/menu/frame icon.png"; // path for the frame icon
+        ImageIcon frameIcon = new ImageIcon(Objects.requireNonNull(Main.class.getResource(frameIconPath))); // creating icon for the frame
         //
 
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // lets the user close the window when you hit x
         window.setResizable(true); // allows user to resize window
         window.setTitle("Food Sins"); /// sets title
+        window.setIconImage(frameIcon.getImage()); // setting the icon of the frame
 
         window.add(gamePanel); // displays everything
-        window.pack(); // causes window to be sized to fit the preferred size and layouts of its subcomponents (gamepanel)
+        window.pack(); // causes window to be sized to fit the preferred size and layouts of its subcomponents (game panel)
         window.setLocationRelativeTo(null); // displays window at the center of the screen
         window.setVisible(true); // can see the window
-
-        window.setIconImage(frameIcon.getImage()); // setting the icon of the frame
 
         gamePanel.startGameThread();
     }
