6b82ac0ec50127228854cbd3062e833066da9d43
yuanhuli <131415626+yuanhuli@users.noreply.github.com>
2023-12-03 22:14:32 -0500

    position
    position

---
+++ food-sins/src/object/SuperObject.java
@@ -0,0 +1,16 @@
+//Create Object class
+//Jennie
+package object.pkg;
+
+public class SuperObject {
+    public BufferedImage image;
+    public String name;
+    public boolean collision = false;
+    public int worldX, worldY
+    
+    public void draw(Graphics2D g2, GamePanel gp) {
+        
+
+    }
+
+}
